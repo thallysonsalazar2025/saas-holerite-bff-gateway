905ee43e6772888a23d51688eeb2ebf31c0e513c
thallysonsalazar2025 <thallyson.salazar@gmail.com>
2026-03-11 15:48:44 -0300

    refactor: renomeia componente para sboot-security-base-auth-service

--- src/main/java/org/com/gateway/SbootSecurityBaseAuthServiceApplication.java
+++ src/main/java/org/com/gateway/SbootSecurityBaseAuthServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SaasHoleriteBffGatewayApplication {
+public class SbootSecurityBaseAuthServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SaasHoleriteBffGatewayApplication.class, args);
+        SpringApplication.run(SbootSecurityBaseAuthServiceApplication.class, args);
     }
 }
